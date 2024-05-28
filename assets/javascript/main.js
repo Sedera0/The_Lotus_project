d14282537d9eca136b1848ec0ac2e042ec0322e2
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2024-05-28 17:00:57 +0300

    feat: responsive navbar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,3 +3,24 @@ window.addEventListener("scroll", function () {
     const navbar = document.getElementById("nav_container");
     navbar.classList.toggle("sticky", window.scrollY > 5);
 });
+
+// Toggle Menu Button
+document.addEventListener('DOMContentLoaded', function () {
+    const showBtn = document.getElementById('menu_btn');
+    const menu = document.getElementById('toogle_menu');
+    const closeBtn = document.getElementById('close_menu');
+    const liens = document.querySelectorAll('.toogle_list i')
+    showBtn.addEventListener('click', function () {
+        menu.classList.add('show');
+    });
+
+    closeBtn.addEventListener('click', function () {
+        menu.classList.remove('show');
+    });
+
+    liens.forEach(function (lien) {
+        lien.addEventListener('click', function () {
+            menu.classList.remove('show');
+        });
+    });
+});
