40e58d044cc62f80d051adc00007a14d69b37fd0
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2024-06-07 20:45:15 +0300

    feat: responsive bungalow section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -55,3 +55,29 @@ document.addEventListener('DOMContentLoaded', function(){
 
     animation();
 })
+
+//Bungalow caroussel
+const carousels = document.querySelectorAll('.carousel');
+
+carousels.forEach(carousel => {
+  const flkty = new Flickity( carousel, {
+    imagesLoaded: true,
+    percentPosition: false,
+    pageDots: true,
+    prevNextButtons: true
+  });
+  
+  const imgs = carousel.querySelectorAll('.carousel-cell img');
+  // get transform property
+  const docStyle = document.documentElement.style;
+  const transformProp = typeof docStyle.transform == 'string' ?
+    'transform' : 'WebkitTransform';
+  
+  flkty.on( 'scroll', function() {
+    flkty.slides.forEach( function( slide, i ) {
+      const img = imgs[i];
+      const x = ( slide.target + flkty.x ) * -1/3;
+      img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+    });
+  });
+})
